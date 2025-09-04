00d6784a16cb8d9b29513535202fb21ba6370c72
tngu0466 <tngu0466@student.monash.edu>
2025-09-04 13:34:50 +1000

    Added comments and explainations to type.ts

--- src/type.ts
+++ src/type.ts
@@ -1,30 +1,37 @@
 export {Constants, Birb, Viewport}
 export type { Key, Event, State, Action, Body, View, CsvRow, Pipe, Rect };
 
+//CONSTANTS
+// The overall dimensions of the SVG canvas
 const Viewport = {
     CANVAS_WIDTH: 600,
     CANVAS_HEIGHT: 400,
 } as const;
 
+// The dimensions of the Birb
 const Birb = {
     WIDTH: 42,
     HEIGHT: 30,
 } as const;
 
+// Game constants
 const Constants = {
     PIPE_WIDTH: 60,
-    TICK_RATE_MS: 30, // Might need to change this!
+    TICK_RATE_MS: 30,
     GRAVITY: 1.5, 
     MAX_FALL_RATE: 18,
     INVINCIBLE_MS: 1000,
 } as const;
 
+//INPUT TYPES
+// Keys that the game listens for
 type Key = "Space" | "KeyR";
 
+// DOM events captured from the keyboard
 type Event = 'keydown' | 'keyup';
 
-// State processing
-
+// Geometry Types
+// A rectangle, used for collision detection
 type Rect = Readonly<{ 
     x: number,
     y: number, 
@@ -32,6 +39,8 @@ type Rect = Readonly<{
     h: number,
 }>
 
+// Game Entities
+// Stores the Birb position, velocity, remaining lives, and creation time
 type Body = Readonly<{
     id: string,
     birbX: number,
@@ -41,6 +50,8 @@ type Body = Readonly<{
     createTime: number,
 }>
 
+// A pipe with a vertical gap
+// The `passed` attribute indicates whether the bird has successfully flown through it
 type Pipe = Readonly<{
     id: string,
     x: number,
@@ -50,6 +61,8 @@ type Pipe = Readonly<{
     passed: boolean,
 }>
 
+// Game State
+// The immutable game state tracked on each tick
 type State = Readonly<{
     gameEnd: boolean,
     time: number,
@@ -63,17 +76,22 @@ type State = Readonly<{
     winAt?: number;
 }>;
 
+// Render/View
+// References to the SVG DOM elements needed for rendering
 type View = Readonly<{
     svg: SVGSVGElement,
     birbImg: SVGImageElement,
 }>
 
+// Shape of each row from the input CSV
 type CsvRow = { 
     gap: number,
     height: number,
     delay: number 
 };
 
+// Action interface
+//  This handle user input, collisions
 interface Action{
     apply(s:State): State;
 }
